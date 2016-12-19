c1dd87e9de83dbfc7369eb5c320ee2fee2320003
Lucas Della Bella <dellabella.lucas@gmail.com>
2016-12-18 20:57:07 -0500

    Initial Commit.

---
+++ desktop/src/rpg/syzle/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package rpg.syzle.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import rpg.syzle.SyzleRPG;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SyzleRPG(), config);
+	}
+}
---
+++ ios/src/rpg/syzle/IOSLauncher.java
@@ -0,0 +1,22 @@
+package rpg.syzle;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import rpg.syzle.SyzleRPG;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new SyzleRPG(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
