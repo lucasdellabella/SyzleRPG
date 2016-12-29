8d9ea6225286546a102ce4d3900e97af7df01741
Lucas Della Bella <dellabella.lucas@gmail.com>
2016-12-29 16:38:06 -0500

    Shift bullets to fire from edge of player sprite

--- core/src/rpg/syzle/Constants.java
+++ core/src/rpg/syzle/Constants.java
@@ -8,6 +8,8 @@ public final class Constants {
     public static final int SCREEN_HEIGHT = 480;
     public static final int TILE_WIDTH = 32;
     public static final int TILE_HEIGHT = 32;
+    public static final int PLAYER_WIDTH = 32;
+    public static final int PLAYER_HEIGHT = 32;
     public static final int MIN_ROOM_SIZE = 4;
     public static final int MAX_ROOM_SIZE = 25;
     public static final int MAX_NUM_ROOMS = 8;
--- core/src/rpg/syzle/Model/Bullet.java
+++ core/src/rpg/syzle/Model/Bullet.java
@@ -1,9 +1,11 @@
 package rpg.syzle.Model;
 
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
+
+import static rpg.syzle.Constants.PLAYER_WIDTH;
+import static rpg.syzle.Constants.PLAYER_HEIGHT;
 
 /**
  * Created by lucasdellabella on 12/29/16.
@@ -16,11 +18,22 @@ public class Bullet extends Sprite implements MyDrawable {
 
     public Bullet(Texture texture, Vector2 startPos, Vector2 direction, float speed) {
         super(texture);
-        setBounds(startPos.x, startPos.y, 10, 36);
-        setOriginCenter();
         rotate(direction.angle() - 90);
         this.direction = direction.nor();
         this.speed = speed;
+
+        setBounds(startPos.x, startPos.y, getTexture().getWidth()/10f, getTexture().getHeight()/10f);
+        correctBulletBounds();
+        setOriginCenter();
+    }
+
+    // Should be player width and height, not tile width and happen.
+    // Right now, however, they both happen to be the same
+    private void correctBulletBounds() {
+        setBounds(getX() - getWidth() / 2 + direction.x * PLAYER_WIDTH,
+                getY() + getHeight() / 2 + direction.y * PLAYER_HEIGHT,
+                getWidth(),
+                getHeight());
     }
 
     public void move() {
