633e37ba79b8b6f6413ddf75c22dedba4893c337
joewang704 <joewang704@gmail.com>
2017-01-02 18:42:38 -0500

    Add texture component

---
+++ core/src/rpg/syzle/Components/TextureComponent.java
@@ -0,0 +1,12 @@
+package rpg.syzle.Components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+/**
+ * Created by joe on 1/2/17.
+ */
+
+public class TextureComponent implements Component {
+    public TextureRegion region = null;
+}
